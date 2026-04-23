0908f3d95bb39d0523e2b3183e20c0c341131666
All298Lie <whdudfo333@naver.com>
2026-04-24 03:39:04 +0900

    Initial commit

---
+++ ChatClient/ChatClient.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatClient
+{
+    internal static class ChatClient // ChatClient 클래스
+    {
+        public static void Main() // ChatClient 프로젝트 메인 함수
+        {
+
+        }
+    }
+}
---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatServer
+{
+    internal static class ChatServer // ChatServer 클래스
+    {
+        public static void Main() // ChatServer 프로젝트 메인 함수
+        {
+
+        }
+    }
+}
